0c3639e00ebfc86e37d54b418c9d69b056d19580
Mokidikitla <67385406+Maisha-crypto@users.noreply.github.com>
2026-02-21 09:28:54 +0200

    Add solution for k-means-assignment

---
+++ k-means-assignment/k-means-assignment.py
@@ -0,0 +1,20 @@
+def k_means_assignment(points, centroids):
+    """
+    Assign each point to the nearest centroid.
+    """
+    assignments = []
+    for p in points:
+        # reset the step distances per point
+        step_distances = {}
+        for i, c in enumerate(centroids):
+            # distance to center
+            if (len(p) & len(c)) == 1:
+                step_distances[i] = (p[0] - c[0]) ** 2
+            else:
+                step_distances[i] = (p[0] - c[0]) ** 2 + (p[1] - c[1]) ** 2
+
+        # get the min diatnce
+        nearest_idx = min(step_distances, key=step_distances.get)
+        assignments.append(nearest_idx)
+
+    return assignments
